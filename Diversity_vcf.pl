5d676f92d56f5cd442e2fb0d4463721f82ff583f
xujiabao127 <xujiabao127@163.com>
2020-07-09 21:17:31 +0800

    Update Diversity_vcf.pl

--- Diversity_vcf.pl
+++ Diversity_vcf.pl
@@ -14,7 +14,7 @@ GetOptions(
 		"step:i" =>\$step,
 		"snpnum:i" =>\$snpnum
 		);
-################  perl Diversity_vcf.pl -vcf Chr31.snp_result.vcf.gz -pop1 pop1.txt  -pop2 pop2.txt -window 10000 -step 1000 -snpnum 1 -output  Diversity_Fst.window.xls
+################   
 
 $window||=10000;
 $step||=1000;
